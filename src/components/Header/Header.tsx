510c5c635410b74769bfa53f0400d876af9952e6
Federico Grandi <fgrandi30@gmail.com>
2020-09-02 17:09:37 +0200

    Upload local work

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Header.css'
+
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
+
+export default function Header() {
+  return (
+    <header className="Header">
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Brand href="/">Nikzionario™</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#about">About</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+    </header>
+  )
+}
