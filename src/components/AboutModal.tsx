2515880fff9a799e93ae340fbc9f522634e38338
Federico Grandi <fgrandi30@gmail.com>
2020-09-05 17:22:04 +0200

    feat: add About modal

---
+++ src/components/AboutModal.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal'
+
+interface Props {
+  show: boolean
+  handleClose: () => void
+}
+
+export default function AboutModal({ show, handleClose }: Props) {
+  return (
+    <Modal show={show} onHide={handleClose} size="xl">
+      <Modal.Header closeButton>About</Modal.Header>
+      <Modal.Body style={{ textJustify: 'auto' }}>
+        <h5>Come funziona:</h5>
+        <p>Il sito si aggiorna automaticamente leggendo le descrizioni delle puntate del podcast e trovando le definizioni. Questo processo è quindi automatico e lontano dall'essere perfetto: se dovessero mancare delle definizioni, contattate l'autore del sito.</p>
+        <h5>Formati riconosciuti:</h5>
+        <p>Al fine di rendere più facile l'aggiornamento del sito, sarebbe meglio che il formato del nikzionario all'interno delle note degli episodi rimanesse costante. Lo script che aggiorna le definizioni ragiona così:</p>
+        <ul>
+          <li>
+            <p>Il Nikzionario è preceduto da una linea che comprende <code>+nikzionario</code> o <code>🍕nikzionario</code> (caps insentive, validi anche con uno spazio tra simbolo e parola), oppure che finisce con <code>nikzionario</code>. Se non viene trovata una linea con queste caratteristiche, la puntata non ha un Nikzionario.</p>
+          </li>
+          <li>
+            <p>Il Nikzionario finisce quando ci sono le indicazioni degli ospiti e dell'editor, ovvero una linea che inizia con <code>con:</code> oppure <code>editato da:</code>. Se non viene trovata una linea con queste caratteristiche, finisce con la fine delle note dell'episodio.</p>
+          </li>
+          <li>
+            <p>Ogni "entry" ha una parola e una definizione, separate da un <code>:</code> o, se non ce n'è nessuno, da un <code>-</code>. Se non c'è nessuno dei due simboli, la linea non ha nessuna "entry"</p>
+          </li>
+        </ul>
+        <h5>Problemi?</h5>
+        <p>Il sito è un progetto open source disponibile su GitHub, quindi chi è in grado può contribuire direttamente lì. In caso di problemi potete contattare l'autore del sito tramite le "Issues" di GitHub, oppure tramite il suo profilo.</p>
+        <p>Link:</p>
+        <ul>
+          <li><a href="https://github.com/EndBug/nikzionario">Progetto del sito</a></li>
+          <li><a href="https://github.com/EndBug">Profilo dell'autore</a></li>
+        </ul>
+      </Modal.Body>
+    </Modal >
+  )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
+import AboutModal from './AboutModal'
 
 export default function Header() {
-  return (
-    <header className="Header">
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="/">Nikzionario™</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+  const [aboutVisible, showAbout] = useState(false)
 
-    </header>
+  return (
+    <div>
+      <header className="Header">
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="/">Nikzionario™</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link onClick={() => showAbout(true)}>About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+      </header>
+      <AboutModal show={aboutVisible} handleClose={() => { showAbout(false) }} />
+    </div>
   )
 }
