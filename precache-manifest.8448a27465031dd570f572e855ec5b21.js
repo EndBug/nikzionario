a11a6b4cd11e5f1647da3732eff3b204d934b8cf
Federico Grandi <fgrandi30@gmail.com>
2020-09-05 17:26:10 +0200

    Updates

---
+++ precache-manifest.8448a27465031dd570f572e855ec5b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287102d063959f1157b5e392d1f89e8b",
+    "url": "/nikzionario/index.html"
+  },
+  {
+    "revision": "ce4f58648df71ac2f68b",
+    "url": "/nikzionario/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c734fe34c4133cefedcc",
+    "url": "/nikzionario/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "ce4f58648df71ac2f68b",
+    "url": "/nikzionario/static/js/2.1dab9cf3.chunk.js"
+  },
+  {
+    "revision": "32d9c3460ee62d17fcc60d35208e458b",
+    "url": "/nikzionario/static/js/2.1dab9cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c734fe34c4133cefedcc",
+    "url": "/nikzionario/static/js/main.6149f3e2.chunk.js"
+  },
+  {
+    "revision": "4ba3f9ab72f895fe94cc",
+    "url": "/nikzionario/static/js/runtime-main.2ffbb07d.js"
+  }
+]);
